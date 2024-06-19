f4a54196b507fcd1491c2767219ee4915fe3b85d
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-06-19 19:42:00 +0400

    Nav section created

---
+++ nav/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.{html,js}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+            },
+            colors: {
+                boz: "#F4F4F4",
+            },
+        },
+    },
+    plugins: [],
+};
