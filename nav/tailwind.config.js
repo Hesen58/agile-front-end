4e0ee11bae6f5cd8d20721053a35ceac149927c8
huseynisrafilov17 <huseynisrafilov17@gmail.com>
2024-06-22 13:17:13 +0400

    some changes

--- nav/tailwind.config.js
+++ nav/tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./*.{html,js}"],
-    theme: {
-        extend: {
-            fontFamily: {
-                poppins: ["Poppins", "sans-serif"],
-            },
-            colors: {
-                boz: "#F4F4F4",
-            },
-        },
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        boz: "#F4F4F4",
+        "boz-kimi": "#CCCCCC",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
