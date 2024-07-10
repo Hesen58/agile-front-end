61d9a5219f268feee1e0906f196d48f5232befca
huseynisrafilov17 <huseynisrafilov17@gmail.com>
2024-07-10 16:11:40 +0400

    some updates

--- cards/tailwind.config.js
+++ cards/tailwind.config.js
@@ -9,6 +9,11 @@ module.exports = {
       colors: {
         boz: "#F4F4F4",
         "boz-kimi": "#CCCCCC",
+        yellow: "#F8BD1C",
+        blue: "#4339F2",
+        red: "#FF3838",
+        purple: "#891BE8",
+        green: "#1AD698",
       },
     },
   },
